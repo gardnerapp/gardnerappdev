93cbd52c5facae24719f3eb5523a3ad6c153462c
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2020-11-06 13:17:08 -0500

    precompile assets

--- app/javascript/components/Personal.js
+++ app/javascript/components/Personal.js
@@ -8,15 +8,15 @@ class Personal extends React.Component {
         <div className="carousel-container">
             <Carousel>
                 <Carousel.Item>
-                  <img src='assets/kevin-ku-w7ZyuGYNpRQ-unsplash.jpg' className="d-block w-100 carousel-img"
+                  <img src='public/assets/kevin-ku-w7ZyuGYNpRQ-unsplash.jpg' className="d-block w-100 carousel-img"
                   alt="first"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src="assets/chris-ried-ieic5Tq8YMk-unsplash.jpg" className="d-block w-100 carousel-img"
+                    <img src="public/assets/chris-ried-ieic5Tq8YMk-unsplash.jpg" className="d-block w-100 carousel-img"
                          alt="second"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src="assets/maxwell-nelson-taiuG8CPKAQ-unsplash.jpg" className="d-block w-100 carousel-img"
+                    <img src="public/assets/maxwell-nelson-taiuG8CPKAQ-unsplash.jpg" className="d-block w-100 carousel-img"
                     alt="third" />
                 </Carousel.Item>
             </Carousel>
