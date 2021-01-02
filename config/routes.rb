ea941222d9afd30ba0f791952309f39e67d77a3a
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-01-02 15:32:06 -0500

    contact scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   root 'home#home'
-  get '/contact', to: 'home#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
