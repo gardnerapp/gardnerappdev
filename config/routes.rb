148c8e49815f793a7ee01236db44a6d2dd439b12
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-04-01 12:55:07 -0400

    remove devise registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, path: 'SuperCoolTechBlog'
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   resources :contacts
   root 'home#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
